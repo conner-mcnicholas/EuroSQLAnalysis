7e0f9589548fc6d12b73ac62e31fe852d6027efd
conner-mcnicholas <connermcnicholas@gmail.com>
2022-02-11 03:58:29 -0800

    removing uFEFF from completed question sols

--- questions/sql_q8_sol.sql
+++ questions/sql_q8_sol.sql
@@ -1,5 +1,5 @@
 SELECT match_no from penalty_shootout
-where `﻿kick_id` = (select max(`﻿kick_id`) from penalty_shootout);
+where ﻿kick_id = (select max(﻿kick_id) from penalty_shootout);
 /*
 +----------+
 | match_no |
--- questions/sql_q9_sol.sql
+++ questions/sql_q9_sol.sql
@@ -1,6 +1,6 @@
 select player_name from player_mast
 where team_id = 1208 AND
-`player_id` in
+player_id in
 (SELECT player_gk from match_details where play_stage = 'G');
 /*
 +--------------+
