4bc2dc23cc7904b89d049fc32b0008ddabc2fb2e
connermcnicholas <38020254+conner-mcnicholas@users.noreply.github.com>
2022-03-04 06:09:06 -0800

    Update sql_q17_sol.sql

--- question_solutions/sql_q17_sol.sql
+++ question_solutions/sql_q17_sol.sql
@@ -2,17 +2,13 @@
 17. Write a SQL query to find the country where the most assistant referees come from,
 and the count of the assistant referees.
 */
-WITH refcounts AS (SELECT c.country_name,COUNT(r.referee_name) AS num_refs
-FROM referee_mast AS r
-LEFT JOIN soccer_country AS c
-ON r.country_id = c.country_id
-GROUP BY 1)
-SELECT country_name,num_refs FROM refcounts WHERE
-num_refs = (SELECT MAX(num_refs) FROM refcounts);
+WITH refcounts AS (SELECT c.country_name,COUNT(r.ass_ref_name) AS num_refs FROM asst_referee_mast AS r,soccer_country AS c 
+WHERE r.country_id = c.country_id GROUP BY 1) 
+SELECT country_name,num_refs FROM refcounts WHERE num_refs = (SELECT MAX(num_refs) FROM refcounts);
 /*
 +--------------+----------+
 | country_name | num_refs |
 +--------------+----------+
-| England      |        2 |
+| England      |        4 |
 +--------------+----------+
-*?
+*/
