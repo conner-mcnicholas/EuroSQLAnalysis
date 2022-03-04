6fe086ceddbc6fc5a57879f66bb972dddfa738f3
connermcnicholas <38020254+conner-mcnicholas@users.noreply.github.com>
2022-03-04 06:00:56 -0800

    Update sql_q13_sol.sql

--- question_solutions/sql_q13_sol.sql
+++ question_solutions/sql_q13_sol.sql
@@ -2,9 +2,9 @@
 13. Write a SQL query to find all the defenders who scored a goal for their teams.
 */
 SELECT p.player_name
-FROM goal_details AS g LEFT JOIN player_mast AS p
-ON g.player_id = p.player_id
-WHERE p.posi_to_play = 'DF';
+FROM goal_details AS g,player_mast AS p
+WHERE g.player_id = p.player_id
+AND p.posi_to_play = 'DF';
 /*
 +------------------------+
 | player_name            |
