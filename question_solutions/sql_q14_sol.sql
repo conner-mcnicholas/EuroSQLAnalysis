8188a61517220bbbe46ea0929e7907109335ac6e
connermcnicholas <38020254+conner-mcnicholas@users.noreply.github.com>
2022-03-04 06:00:20 -0800

    Update sql_q14_sol.sql

--- question_solutions/sql_q14_sol.sql
+++ question_solutions/sql_q14_sol.sql
@@ -2,34 +2,30 @@
 14. Write a SQL query to find referees and the number of bookings they made for the
 entire tournament. Sort your answer by the number of bookings in descending order.
 */
-SELECT r.referee_name,COUNT(p.player_id) AS bookings FROM
-match_mast AS m LEFT JOIN player_booked AS p
-ON m.match_no = p.match_no LEFT JOIN
-referee_mast AS r ON
-m.referee_id = r.referee_id
-GROUP BY 1
-ORDER BY 2 desc;
+select rm.referee_name, count(*) count  from  player_booked pb,         match_mast mm,       referee_mast rm  where pb.match_no = mm.match_no and
+mm.referee_id = rm.referee_id  group by rm.referee_name   order by count(*) desc;
 /*
-+-------------------------+----------+
-| referee_name            | bookings |
-+-------------------------+----------+
-| Mark Clattenburg        |       21 |
-| Nicola Rizzoli          |       20 |
-| Milorad Mazic           |       13 |
-| Bjorn Kuipers           |       12 |
-| Viktor Kassai           |       12 |
-| Sergei Karasev          |       12 |
-| Damir Skomina           |       12 |
-| Cuneyt Cakir            |       11 |
-| Jonas Eriksson          |       11 |
-| Pavel Kralovec          |       11 |
-| Szymon Marciniak        |       10 |
-| Carlos Velasco Carballo |       10 |
-| Martin Atkinson         |        9 |
-| Ovidiu Hategan          |        9 |
-| Felix Brych             |        9 |
-| Svein Oddvar Moen       |        8 |
-| William Collum          |        8 |
-| Clement Turpin          |        3 |
-+-------------------------+----------+
++-------------------------+-------+
+| referee_name            | count |
++-------------------------+-------+
+| Mark Clattenburg        |    21 |
+| Nicola Rizzoli          |    20 |
+| Milorad Mazic           |    13 |
+| Bjorn Kuipers           |    12 |
+| Viktor Kassai           |    12 |
+| Sergei Karasev          |    12 |
+| Damir Skomina           |    12 |
+| Cuneyt Cakir            |    11 |
+| Jonas Eriksson          |    11 |
+| Pavel Kralovec          |    11 |
+| Szymon Marciniak        |    10 |
+| Carlos Velasco Carballo |    10 |
+| Martin Atkinson         |     9 |
+| Ovidiu Hategan          |     9 |
+| Felix Brych             |     9 |
+| Svein Oddvar Moen       |     8 |
+| William Collum          |     8 |
+| Clement Turpin          |     3 |
++-------------------------+-------+
+
 */
